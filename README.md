e43499674961db3fd951bf624844103e83823eb1
Nexxee <nexus633@googlemail.com>
2018-05-17 21:44:45 +0200

    Update
    Add Travis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# LinuxServerManager
+# LinuxServerManager (LSM)
+[![Build Status](https://travis-ci.org/Nexus633/LinuxServerManager.svg?branch=master)](https://travis-ci.org/Nexus633/LinuxServerManager)
+[![Build Status](https://travis-ci.org/Nexus633/LinuxServerManager.svg?branch=develop)](https://travis-ci.org/Nexus633/LinuxServerManager)
---
+++ lsm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#==============================================================================
+#
+#         FILE:  lsm.sh
+#
+#        USAGE:  
+#
+#  DESCRIPTION:  
+#                
+#                 
+#
+#      OPTIONS:  see function ’usage’ below
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Andre Rohlf, Andre_rohlf@msn.com
+#      COMPANY:  ---
+#      VERSION:  0.1
+#      CREATED:  17.05.2018 - 21:00:00
+#     REVISION:  17.05.2018
+#==============================================================================
